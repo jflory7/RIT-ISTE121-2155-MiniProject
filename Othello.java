401f5f9f8e3e38cf96d872fc88b08bc580cd2204
Tim Endersby <endersbyt@gmail.com>
2016-02-15 10:38:30 -0500

    Menu Bar
    Started adding Menu Bar

--- Othello.java
+++ Othello.java
@@ -7,11 +7,26 @@ import javax.swing.*;
 * ISTE 121.01
  */
 public class Othello extends JFrame {
+    private JMenuItem jmiNewGame;
     public static void main(String[] args){
         new Othello();
     }
 
-    public Othello{
-
+    public Othello(){
+        JMenuBar menuBar = new JMenuBar();
+            JMenu jmFile = new JMenu("File");
+                jmiNewGame = new JMenuItem("New Game");
+                jmFile.add(jmiNewGame);
+            menuBar.add(jmFile);
+        add(menuBar, BorderLayout.NORTH);
+        
+        //add(new GameBoard(), BorderLayout.CENTER);
+        //add(new ScoreBoard(), BorderLayout.EAST);
+        
+        pack();
+        setTitle("Othello");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
 }
