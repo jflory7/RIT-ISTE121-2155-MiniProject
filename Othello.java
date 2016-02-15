b44871b504e7519079e96664c0b088a1936c1262
endersbyt <endersbyt@gmail.com>
2016-02-15 10:24:44 -0500

    Othello main class
    Make othello main class and begin constructor

---
+++ Othello.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+/*
+* Mini Project 1 - Othello
+* Timothy Endersby and Justin ory
+* ISTE 121.01
+ */
+public class Othello extends JFrame {
+    public static void main(String[] args){
+        new Othello();
+    }
+
+    public Othello{
+
+    }
+}
