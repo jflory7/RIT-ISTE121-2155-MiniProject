9f4e78ecb80b84f4c5bc10e5566053a197e4f1de
Justin W. Flory <me@justinwflory.com>
2016-03-12 23:48:52 -0500

    Fix copyright header (desktop wasn't configured same as laptop)

--- src/main/java/GamePiece.java
+++ src/main/java/GamePiece.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Justin W. Flory
+ * Copyright (c) 2016 Justin W. Flory, Timothy Endersby
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
